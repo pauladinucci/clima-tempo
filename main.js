322910169d2bd1be69f0f42c036e3e873e0e9a12
pauladinucci <paulaldinucci@gmail.com>
2023-02-06 00:41:15 -0300

    criação e edição do javascript

---
+++ main.js
@@ -0,0 +1,64 @@
+document.querySelector(".busca").addEventListener("submit", async (event) => {
+    event.preventDefault(); //retura os efeitos padrões
+
+    let input = document.querySelector("#searchInput").value;
+
+    if (input !== "") {
+        clearInfo();
+        showWarning("Carregando...");
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+            input
+        )}&appid=2ee7093f489da51e01b5c9dbcfc978e8`;
+
+        //fazendo a requisição a API
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+            showInfo({
+                name:json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearInfo();
+            showWarning("Localização não encontrada.");
+        }
+    } else {
+        clearInfo();
+    }
+});
+
+function showInfo(json) {
+    showWarning("");
+    document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector(".tempInfo").innerHTML = `{json.temp} <sup>ºC</sup>`;
+    document.querySelector(
+        ".ventoInfo"
+    ).innerHTML = `${json.windSpeed} <span>Km/h</span>`;
+
+    document
+    .querySelector(".temp img")
+    .setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
+
+    document.querySelector(".ventoPonto").getElementsByClassName.transform = `rotate(${
+        json.windAngle = 90
+    }deg)`;
+    document.querySelector(".resultado").style.display = "block";
+}
+
+function clearInfo() {
+    showWarning("");
+    document.querySelector(".resultado").style.display = "none";
+}
+
+const showWarning = (msg) => {
+    document.querySelector(".aviso").innerHTML = msg;
+};
